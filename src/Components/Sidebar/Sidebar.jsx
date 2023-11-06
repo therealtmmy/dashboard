8b6f469bba613af9fb423fd16ca887680336da72
therealtmmy <oluwatimilehinsamson@gmail.com>
2023-11-06 23:06:43 +0100

    Building the UI

---
+++ src/Components/Sidebar/Sidebar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import "./Sidebar.css"
+
+const Sidebar = () => {
+  return (
+    <>
+    <div className='Sidebar'>
+        <div className='Side1'>
+            <img src="https://media.licdn.com/dms/image/D4E03AQE75pfK8Jj8dQ/profile-displayphoto-shrink_400_400/0/1679484776972?e=2147483647&v=beta&t=J-_C2x46QaNlORs56QirtG1Evdl9k2gWKpfW5e_A8sg" alt="Winner Chaper Logo" />
+            <p>Winners Chapel, Gaga</p>
+        </div>
+
+        <div className='line'></div>
+    </div>
+    </>
+  )
+}
+
+export default Sidebar
