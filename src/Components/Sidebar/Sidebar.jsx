2a3161de824405f1c90bf10a24d8f5f4f918ce62
therealtmmy <oluwatimilehinsamson@gmail.com>
2023-11-07 00:33:42 +0100

    Built the side bar

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -1,19 +1,71 @@
-import React from 'react'
-import "./Sidebar.css"
+import React from "react";
+import "./Sidebar.css";
+import {
+  FaHome,
+  FaChalkboardTeacher,
+  FaUser,
+  FaCreditCard,
+  FaCog,
+  FaChartBar,
+  FaExternalLinkAlt,
+} from "react-icons/fa";
 
 const Sidebar = () => {
+  const iconData = [
+    {
+      icon: FaHome,
+      name: "Dashboard",
+    },
+    {
+      icon: FaChalkboardTeacher,
+      name: "Teachers",
+    },
+    {
+      icon: FaUser,
+      name: "Students/classes",
+    },
+    {
+      icon: FaCreditCard,
+      name: "Billing",
+    },
+    {
+      icon: FaCog,
+      name: "Settings and Profile",
+    },
+    {
+      icon: FaChartBar,
+      name: "Exams",
+    },
+  ];
+
   return (
     <>
-    <div className='Sidebar'>
-        <div className='Side1'>
-            <img src="https://media.licdn.com/dms/image/D4E03AQE75pfK8Jj8dQ/profile-displayphoto-shrink_400_400/0/1679484776972?e=2147483647&v=beta&t=J-_C2x46QaNlORs56QirtG1Evdl9k2gWKpfW5e_A8sg" alt="Winner Chaper Logo" />
-            <p>Winners Chapel, Gaga</p>
+      <div className="Sidebar">
+        <div className="Side1">
+          <img
+            src="https://i.pinimg.com/originals/f9/6a/26/f96a261e5a60d7d66b36e2850e3eb19b.png"
+            alt="Winner Chaper Logo"
+          />
+          <p>Udemy Accelerate</p>
         </div>
 
-        <div className='line'></div>
-    </div>
-    </>
-  )
-}
+        <div className="line"></div>
 
-export default Sidebar
+        <div className="Side2">
+          {iconData.map((value, index) => (
+            <button key={index}>
+              {React.createElement(value.icon, { style: { fontSize: "19px" } })}{" "}
+              {value.name}
+            </button>
+          ))}
+
+          <button className="Feature">
+            <FaExternalLinkAlt style={{ fontSize: "19px" }} /> Feature
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Sidebar;
