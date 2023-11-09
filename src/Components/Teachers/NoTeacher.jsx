30936d6d4783e5cd9c67f93214256aee88b4d632
therealtmmy <oluwatimilehinsamson@gmail.com>
2023-11-09 14:13:43 +0100

    Built Teachers Error Page

---
+++ src/Components/Teachers/NoTeacher.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./NoTeacher.css";
+
+const NoTeacher = () => {
+  return (
+    <div className="NoTeacher">
+      <h3>No Teachers at this time</h3>
+      <p>Teachers will appear here after they enroll in your school</p>
+    </div>
+  );
+};
+
+export default NoTeacher;
--- src/Components/Teachers/Teachers.jsx
+++ src/Components/Teachers/Teachers.jsx
@@ -1,9 +1,42 @@
 import React from "react";
+import { FaRegBell, FaSearch, FaSortDown } from "react-icons/fa";
+import "./Teachers.css";
+import NoTeacher from "./NoTeacher";
 
 const Teachers = () => {
   return (
-    <div>
-      <p></p>
+    <div className="Teacher">
+      <div className="TeacherHeader">
+        <div className="TeacherNotification">
+          <FaRegBell className="Bell" />
+          <button className="TeacherButton">Log out</button>
+        </div>
+      </div>
+
+      <div className="HeadingTwo">
+        <p>Teachers</p>
+
+        <div>
+          <button>Add Teachers</button>
+        </div>
+      </div>
+
+      <div className="InputDiv">
+        <div>
+          <select name="" id="Filter">
+            <option value="">
+              Filter <FaSortDown />
+            </option>
+            <option value="">Class</option>
+            <option value="">Gender</option>
+          </select>
+        </div>
+        <div className="SecInput">
+          <FaSearch className="SearchIcon" />
+          <input type="text" placeholder="Search for a teacher by name" />
+        </div>
+      </div>
+      <NoTeacher />
     </div>
   );
 };
